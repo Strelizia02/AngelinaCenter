2b8b51b008719053ecd0fce6bc4777b934d59c56
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-13 15:28:02 +0800

    Update and rename UserPropertyController.java to UserController.java

--- src/main/java/top/strelitzia/controller/UserController.java
+++ src/main/java/top/strelitzia/controller/UserController.java
@@ -15,10 +15,10 @@ import top.strelitzia.vo.JsonResult;
  * @Date 2022/12/12
  * 查询用户个人资产接口
  **/
-@RequestMapping("userProperty")
+@RequestMapping("user")
 @RestController
 @Slf4j
-public class UserPropertyController {
+public class UserController {
 
     @Autowired
     private UserPropertyService userPropertyService;
@@ -26,12 +26,33 @@ public class UserPropertyController {
     /**
      * 查询某人的详细资产数据
      * @param id 登录人id
-     * @return 登录人信息
+     * @return 登录人的资产信息
      */
     @Token
-    @PostMapping("getUserProperty")
+    @GetMapping("getUserProperty")
     public JsonResult<UserProperty> getUserProperty(@RequestParam String id) {
         return JsonResult.success(userPropertyService.getUserProperty(id));
     }
 
+    /**
+     * 查询某人的详细数据
+     * @param id 登录人id
+     * @return 登录人的个人信息
+     */
+    @Token
+    @GetMapping("getUserInfo")
+    public JsonResult<UserProperty> getUserInfo(@RequestParam String id) {
+        return JsonResult.success(userPropertyService.getUserProperty(id));
+    }
+    
+    /**
+     * 修改某人的详细数据
+     * @param userInfo 登录人个人信息
+     * @return 登录人的个人信息
+     */
+    @Token
+    @PostMapping("editUserInfo")
+    public JsonResult<UserProperty> editUserInfo(@Requestbody UserInfo userInfo) {
+        return JsonResult.success(userPropertyService.getUserProperty(id));
+    }
 }

cda506ab74484c061b2947878f65960ab6cd0c69
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-13 10:36:38 +0800

    Create LoginInfo.java

---
+++ src/main/java/top/strelitzia/models/LoginInfo.java
@@ -0,0 +1,24 @@
+package top.strelitzia.models;
+
+public class LoginInfo {
+    
+    private Boolean ok;
+    
+    private UserInfo userInfo;
+
+    public Boolean getOk() {
+        return ok;
+    }
+
+    public void setOk(Boolean ok) {
+        this.ok = ok;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+}

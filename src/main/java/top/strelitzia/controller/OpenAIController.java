3e3da1bd963af43eb5acf5d50322c9013b56f977
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-13 20:00:28 +0800

    Update OpenAIController.java

--- src/main/java/top/strelitzia/controller/OpenAIController.java
+++ src/main/java/top/strelitzia/controller/OpenAIController.java
@@ -30,6 +30,8 @@ public class OpenAIController {
     @PostMapping("getApiKey")
     public JsonResult<String> openAiApi(@RequestBody OpenAiModel body, @RequestHeader(value = "apiKey", required = false) String apiKey) {
         //TODO Header里携带id参数就可以了，直接用id校验
+        
+        //TODO 每次调用完了，要给用户统计使用量，最好也做个图表
         String s = openAIService.sendChatGPT(body, apiKey);
         return JsonResult.success(s);
     }

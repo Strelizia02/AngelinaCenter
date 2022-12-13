9b557f783477723d033ffee0c1a497744a714a1f
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-13 20:03:42 +0800

    Update FileController.java

--- src/main/java/top/strelitzia/controller/FileController.java
+++ src/main/java/top/strelitzia/controller/FileController.java
@@ -30,9 +30,9 @@ public class FileController {
     public JsonResult<UserProperty> downloadFile(@RequestParam String fileId) {
         //TODO 首先要有定时任务，检查git上数据版本，然后使用JGit拉取数据。
         
-        //TODO 分文件记录每个人的下载次数（文件名、user、下载次数），每次数据更新后，把下载次数清空
+        //TODO 分文件记录每个人的下载次数（文件名、user、下载次数），每次数据更新后，把下载次数清空，记得要在RabbitMQ里发送消息，通知用户更新
         
-        //TODO 搞到本地文件里就行了，省钱
+        //TODO 搞到本地文件里就行了，省钱。
         return JsonResult.success(userPropertyService.getUserProperty(fileId));
     }
 

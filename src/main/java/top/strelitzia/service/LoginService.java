7dd5b24a962f54a9d9c7d85878c63946abc57451
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-14 15:39:06 +0800

    Update LoginService.java

--- src/main/java/top/strelitzia/service/LoginService.java
+++ src/main/java/top/strelitzia/service/LoginService.java
@@ -18,6 +18,9 @@ public class LoginService {
 
     @Autowired
     private RSAUtil rsaUtil;
+    
+    @Autowired
+    private TokenUtil tokenUtil;
 
     @Autowired
     private UserMapper userMapper;
@@ -25,15 +28,19 @@ public class LoginService {
     public static final Map<String, Captcha> captchaMap = new HashMap<>();
 
     public LoginInfo pwdLogin(UserInfo userInfo) {
-        String id = userInfo.getId();
+        //先从登录信息里提取id
+        String name = userInfo.getName();
+        String id = loginMapper.selectIdByName(name);
+
         String pwd = rsaUtil.decryptWithPrivate(userInfo.getPwd());
         LoginInfo loginInfo = new LoginInfo();
 
+        //先对密码进行解密操作，然后对比Md5
         if (pwd != null) {
             UserInfo userInfo1 = userMapper.selectUserInfo(id);
             if (pwd.equals(DigestUtils.md5DigestAsHex(userInfo1.getPwd().getBytes()))) {
                 loginInfo.setOk(true);
-                loginInfo.setToken();
+                loginInfo.setToken(tokenUtil.createToken(id));
                 loginInfo.setUserInfo(userInfo1);
             } else {
                 loginInfo.setOk(false);
@@ -47,12 +54,17 @@ public class LoginService {
     /**
      * 查看有没有发验证码
      */
-    public LoginInfo captchaLogin(String id) {
+    public LoginInfo captchaLogin(String qq) {
+        //先通过qq查到要登录哪个id
         LoginInfo loginInfo = new LoginInfo();
-        if (captchaMap.containsKey(id)) {
-            if (captchaMap.get(id).getIsSend()) {
+        if (captchaMap.containsKey(qq)) {
+            //有没有验证过
+            if (captchaMap.get(qq).getIsSend()) {
+                String id = loginMapper.selectIdByQq(qq);
                 loginInfo.setOk(true);
+                loginInfo.setToken(tokenUtil.createToken(id));
                 loginInfo.setUserInfo(userMapper.selectUserInfo(id));
+                captchaMap.remove(qq)
             } else {
                 loginInfo.setOk(false);
             }
@@ -65,44 +77,55 @@ public class LoginService {
     /**
      * 接受验证码方法
      */
-    public Boolean receivedCaptcha(String id, String captcha) {
-        if (captchaMap.containsKey(id)) {
-            Captcha captchaObj = captchaMap.get(id);
-
-            if (captchaObj.getCaptcha().equals(captcha)) {
-                captchaObj.setIsSend(true);
-            }
+    public Boolean receivedCaptcha(CaptchaRecive captchaRecive) {
+        //首先对比qq，id，验证码是否一致
+        if (captchaMap.containsKey(captchaRecive.getQq()) 
+                && loginMapper.selectIdbyQq(qq).equals(captchaRecive.getId())
+                && captchaObj.getCaptcha().equals(captchaRecive.getRaptcha())) {
+            Captcha captchaObj = captchaMap.get(captchaRecive.getQq());
+            captchaObj.setIsSend(true);
             return true;
         } else {
             return false;
         }
     }
 
-    public String creatCaptcha(String id) {
+    public String creatCaptcha(String qq) {
         StringBuilder sb = new StringBuilder();
         SecureRandom random = new SecureRandom();
         for (int i = 0; i < 6; i++) {
             sb.append(random.nextInt(10));
         }
-
-        captchaMap.put(id, new Captcha(id, sb.toString()));
-
+        captchaMap.put(id, new Captcha(qq, sb.toString()));
         return sb.toString();
     }
 
     /**
-     * 删除验证码方法
+     * 定时删除验证码方法，这里考虑换用redis
      */
     public void removeCaptcha() {
-        for (String id: captchaMap.keySet()) {
-            if (System.currentTimeMillis() - captchaMap.get(id).getTimestamp() >= 60 * 1000) {
-                captchaMap.remove(id);
+        for (String qq: captchaMap.keySet()) {
+            if (System.currentTimeMillis() - captchaMap.get(qq).getTimestamp() >= 60 * 1000) {
+                captchaMap.remove(qq);
             }
         }
     }
 
-    public UserInfo register(UserInfo userInfo) {
+    public LoginInfo register(UserInfo userInfo) {
+        //用户只填写账号密码
+        LoginInfo loginInfo = new LoginInfo();
+        String id = loginMapper.selectIdByName(userInfo.getName());
+        if (id == null) {
+            loginInfo.setOk(false);
+            loginInfo.setToken("当前用户名已被占用");
+            return loginInfo;
+        }
+        userInfo.setIsRegister(true);
+        userInfo.setIsBot(false);
         userMapper.insertUserInfo(userInfo);
-        return userInfo;
+        loginInfo.setUserInfo(userMapper.selectUserInfoByName(userInfo.getName()));
+        loginInfo.setOk(true);
+        loginInfo.setToken(tokenUtil.createToken(id));
+        return new Info(true, "注册成功");
     }
 }

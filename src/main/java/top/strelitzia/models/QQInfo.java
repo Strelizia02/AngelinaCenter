cb9f3e5f32a16bb488a9bea73bdfc1747c9f4257
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-15 14:50:08 +0800

    Create QQInfo.java

---
+++ src/main/java/top/strelitzia/models/QQInfo.java
@@ -0,0 +1,15 @@
+package top.strelitzia.models;
+
+import lombok.Data;
+
+@Data
+public class QQInfo {
+
+    private String qq;
+
+    private String frame;
+
+    private String type;
+
+    private Boolean isOnline;
+}

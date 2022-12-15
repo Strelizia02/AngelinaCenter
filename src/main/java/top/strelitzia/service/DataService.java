8e46578a88081226298ca517ce04dffc7675afb9
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-15 12:07:52 +0800

    Update DataService.java

--- src/main/java/top/strelitzia/service/DataService.java
+++ src/main/java/top/strelitzia/service/DataService.java
@@ -12,14 +12,46 @@ public class DataService {
     }
   
     public Info editNotice(String token, Integer id, String text, MultipartFile img) {
-      Integer id = tokenUtil.getTokenId(token);
-      UserInfo userInfo = userMapper.selectUserById(id); 
-      
-      if (userInfo.getIsAdmin()) {
-        if (userInfo.getImg != null) {
-          String path = fileService.saveFile(Img);
+        Integer id = tokenUtil.getTokenId(token);
+        UserInfo userInfo = userMapper.selectUserById(id); 
+
+        if (userInfo.getIsAdmin()) {
+            if (userInfo.getImg != null) {
+                String path = fileService.saveFile(Img);
+            } 
+            noticeMapper.updateNotice(id, text, img);
+            return new Info(true, "修改成功");
+        } else {
+            return new Info(false, "您没有权限");
         }
-        noticeMapper.updateNotice(id, text, img);
-        return list;
+    }
+    
+    public Info createNotice(String token, String text, MultipartFile img) {
+        Integer id = tokenUtil.getTokenId(token);
+        UserInfo userInfo = userMapper.selectUserById(id); 
+
+        if (userInfo.getIsAdmin()) {
+            if (userInfo.getImg != null) {
+                String path = fileService.saveFile(Img);
+            } 
+            noticeMapper.insertNotice(text, img);
+            return new Info(true, "创建成功");
+        } else {
+            return new Info(false, "您没有权限");
+        }
+    }
+    
+    public Info deleteNotice(String token, Integer noticeId) {
+        Integer id = tokenUtil.getTokenId(token);
+        UserInfo userInfo = userMapper.selectUserById(id); 
+
+        if (userInfo.getIsAdmin()) {
+            Notice notice = noticeMapper.selectNoticeById(noticeId);  
+            new File(notice.getImg()).deleteFile();
+            noticeMapper.deleteNotice(noticeId);
+            return new Info(true, "删除成功");
+        } else {
+            return new Info(false, "您没有权限");
+        }
     }
 }

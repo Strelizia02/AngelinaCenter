6684e478c76c8bacc5242b50f0e225aa83650bda
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-15 15:59:58 +0800

    Update DataController.java

--- src/main/java/top/strelitzia/controller/DataController.java
+++ src/main/java/top/strelitzia/controller/DataController.java
@@ -28,47 +28,6 @@ public class DataController {
     private NoticeService noticeService;
 
     /**
-     * 获取全部公告信息
-     * @return 公告信息
-     */
-    @GetMapping("getNotice")
-    public JsonResult<List<Notice>> getNotice() {
-        return JsonResult.success(noticeService.getNotice());
-    }
-  
-    /**
-     * 编辑某条公告信息
-     * @return 公告信息
-     */
-    @Token
-    @PostMapping("editNotice")
-    public JsonResult<Notice> editNotice(@RequestHeader String token, @ApiParam(value = "img") @RequestParam MultipartFile img, @ApiParam(value = "id") @RequestParam Integer id, @ApiParam(value = "text") @RequestParam String text) {
-        return JsonResult.success(noticeService.editNotice(token, id, text, img));
-    }
-
-    /**
-     * 删除某条公告信息
-     * @return true/false
-     */
-    @Token
-    @DeleteMapping("deleteNotice")
-    public JsonResult<Boolean> deleteNotice(@RequestHeader String token, @RequestParam Integer noticeId) {
-      //TODO 这里要做权限校验，看看这个人有没有权限删除
-        return JsonResult.success(noticeService.deleteNotice(token, noticeId));
-    }
-  
-    /**
-     * 创建一条公告
-     * @return 公告
-     */
-    @Token
-    @PostMapping("createNotice")
-    public JsonResult<Notice> createNotice(@RequestHeader String token, @ApiParam(value = "img") @RequestParam MultipartFile img, @ApiParam(value = "text") @RequestParam String text) {
-      //TODO 这里要做权限校验，看看这个人有没有权限创建
-        return JsonResult.success(noticeService.createNotice(token, text, img));
-    }
-  
-    /**
      * 心跳接口
      * @param bot bot信息，包含了一个botid和其下账号的在线信息
      */

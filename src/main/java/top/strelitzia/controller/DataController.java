ad07473272dfc03f99188465b5daa8e3e05a9257
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-15 15:52:29 +0800

    Update DataController.java

--- src/main/java/top/strelitzia/controller/DataController.java
+++ src/main/java/top/strelitzia/controller/DataController.java
@@ -71,33 +71,32 @@ public class DataController {
     /**
      * 心跳接口
      * @param bot bot信息，包含了一个botid和其下账号的在线信息
-     * @return true/false
      */
     @PostMapping("heartBeats")
-    public JsonResult<Boolean> heartBeats(@RequestBody Bot bot) {
+    public JsonResult<String> heartBeats(@RequestBody Bot bot) {
       //TODO 心跳接口，要传过来Bot的id，所有账号的在线情况，返回Botid（第一次心跳需要生成id）
-        return JsonResult.success();
+        return JsonResult.success(dataService.heartBeats(bot));
     }
   
     /**
-     * 获取全部Bot的列表
+     * 获取某个账号下所有Bot的列表
      * @return 全部信息
      */
     @Token
     @PostMapping("getBotList")
-    public JsonResult<List<Bot>> getBotList() {
+    public JsonResult<List<Bot>> getBotList(@RequestHeader String token) {
         //TODO 数据库里记录的是每个账号的最后一次心跳时间，超过五分钟没有心跳就算离线
-        return JsonResult.success();
+        return JsonResult.success(dataService.getBotList(token));
     }
   
     /**
      * 获取Bot图表数据
      * @return 在线几个、离线几个、一共几个
      */
-    @PostMapping("getBotBoard")
+    @GetMapping("getBotBoard")
     public JsonResult<BotData> getBotBoard() {
         //TODO 统计一下，返回一个数据量少一点的
-        return JsonResult.success();
+        return JsonResult.success(dataService.getBotBoard());
     }
   
     /**
@@ -105,17 +104,26 @@ public class DataController {
      * @return true/false
      */
     @PostMapping("pushData")
-    public JsonResult<Boolean> pushData(@RequestBody List<Function> list) {
-        return JsonResult.success();
+    public JsonResult<Boolean> pushData(@RequestHeader String token, @RequestBody List<Function> list) {
+        return JsonResult.success(dataService.pushData(token, list));
     }
   
     /**
      * 获取全部功能的列表
      * @return 每个功能的详细调用次数
      */
-    @Token
     @PostMapping("getFuncList")
     public JsonResult<List<Function>> getFuncList() {
-        return JsonResult.success();
+        return JsonResult.success(dataService.getFuncList());
+    }
+    
+    /**
+     * 获取某个人的功能的列表
+     * @return 每个功能的详细调用次数
+     */
+    @Token
+    @PostMapping("getSomeOneFuncList")
+    public JsonResult<List<Function>> getSomeOneFuncList(@RequestHeader String token) {
+        return JsonResult.success(dataService.getSomeOneFuncList(token));
     }
 }

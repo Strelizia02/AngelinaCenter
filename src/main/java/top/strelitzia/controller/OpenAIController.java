59b26894fa3ebe40bbca2edaae93889539236085
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-19 17:53:28 +0800

    Update OpenAIController.java

--- src/main/java/top/strelitzia/controller/OpenAIController.java
+++ src/main/java/top/strelitzia/controller/OpenAIController.java
@@ -17,6 +17,7 @@ import top.strelitzia.vo.JsonResult;
 @RequestMapping("openai")
 @RestController
 @Slf4j
+@Api("O盘AI的转发相关接口")
 public class OpenAIController {
 
     @Autowired
@@ -28,6 +29,7 @@ public class OpenAIController {
      * @return 返回结果
      */
     @PostMapping("getApiKey")
+    @ApiOperation("转发OpenAI，需要传过来BotId")
     public JsonResult<Info> openAiApi(@RequestBody OpenAiModel body, @RequestHeader(value = "botId", required = false) String botId) {
         //QQ号容易伪造，Botid不好伪造
         return JsonResult.success(openAIService.sendChatGPT(body, botId));
@@ -40,6 +42,7 @@ public class OpenAIController {
      */
     @Token
     @PostMapping("addOpenAiToken")
+    @ApiOperation("管理员给某个账号添加token余额")
     public JsonResult<Boolean> addOpenAiToken(@RequestHeader(value = "Authorization", required = false) String token, @RequestParam String id, @RequestParam Long num) {
         return JsonResult.success(openAIService.buyOpenAiToken(token,id, num));
     }

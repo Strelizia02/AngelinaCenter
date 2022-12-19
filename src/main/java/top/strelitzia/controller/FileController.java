24677377696a051f82c2f61846f3d64a10fc0c2e
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-19 14:47:01 +0800

    Update FileController.java

--- src/main/java/top/strelitzia/controller/FileController.java
+++ src/main/java/top/strelitzia/controller/FileController.java
@@ -1,39 +1,57 @@
-//package top.strelitzia.controller;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import top.strelitzia.annotation.Token;
-//import top.strelitzia.model.UserInfo;
-//import top.strelitzia.service.UserService;
-//import top.strelitzia.vo.JsonResult;
-//
-//
-///**
-// * @author strelitzia
-// * @Date 2022/12/12
-// * 下载文件专用接口
-// **/
-//@RequestMapping("file")
-//@RestController
-//@Slf4j
-//public class FileController {
-//
-//    @Autowired
-//    private UserService userPropertyService;
-//
-//    @Token
-//    @PostMapping("download")
-//    public JsonResult<UserInfo> downloadFile(@RequestParam String fileId) {
-//        //TODO 首先要有定时任务，检查git上数据版本，然后使用JGit拉取数据。
-//
-//        //TODO 分文件记录每个人的下载次数（文件名、user、下载次数），每次数据更新后，把下载次数清空，记得要在RabbitMQ里发送消息，通知用户更新
-//
-//        //TODO 搞到本地文件里就行了，省钱。
-//        return JsonResult.success(userPropertyService.getUserProperty(fileId));
-//    }
-//
-//}
+package top.strelitzia.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import top.strelitzia.annotation.Token;
+import top.strelitzia.model.UserInfo;
+import top.strelitzia.service.UserService;
+import top.strelitzia.vo.JsonResult;
+
+
+/**
+* @author strelitzia
+* @Date 2022/12/12
+* 下载文件专用接口
+**/
+@RequestMapping("file")
+@RestController
+@Slf4j
+public class FileController {
+
+    @Autowired
+    private UserService userPropertyService;
+  
+    private final String[] fileNameList = new String[]{"character_table.json", "gacha_table.json", "skill_table.json", "building_data.json", "handbook_info_table.json", "charword_table.json",
+                                                  "char_patch_table.json", "item_table.json", "skin_table.json", "battle_equip_table.json", "uniequip_table.json", "enemy_database.json", "data_version.txt"};
+
+    private final String[] pathList = new String[]{"gamedata/excel/character_table.json", "gamedata/excel/gacha_table.json", "gamedata/excel/skill_table.json", "gamedata/excel/building_data.json", "gamedata/excel/handbook_info_table.json", "gamedata/excel/charword_table.json",
+                                                  "gamedata/excel/char_patch_table.json", "gamedata/excel/item_table.json", "gamedata/excel/skin_table.json", "gamedata/excel/battle_equip_table.json", "gamedata/excel/uniequip_table.json", "gamedata/levels/enemydata/enemy_database.json", "gamedata/excel/data_version.txt"};
+
+
+   @PostMapping("download")
+   public ResponseEntity<FileSystemResource> downloadFile(@RequestParam String fileName, @RequestParam String botId) {
+        List<String> botIds = botMapper.selectAllBotId();
+    if (botIds.contains(botId) && fileNameList.contains(fileName)) {
+        File file = new File(fileNameList.get(fileName));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", "attachment; filename=" + System.currentTimeMillis() + ".xls");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Last-Modified", new Date().toString());
+        headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+ 
+    return ResponseEntity
+            .ok()
+            .headers(headers)
+            .contentLength(file.length())
+            .contentType(MediaType.parseMediaType("application/octet-stream"))
+            .body(new FileSystemResource(file));
+   }
+
+
+}
